0c8c6956cfaa4593beed37cbcfc5c7ec85d021dd
Alexi Rahman <alexi.rahman@r76.se>
2016-08-08 19:51:09 +0200

    Lets use proxies if we can

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const rp = require("request-promise");
 
 module.exports = {
 
-  facebook: (url) => {
-    return rp({
+  facebook: (url, proxy) => {
+    const opts = {
       uri: `https://api.facebook.com/method/fql.query?&format=json&query=select%20total_count,like_count,comment_count,share_count,click_count%20from%20link_stat%20where%20url="${encodeURIComponent(url)}"`,
       json: true
-    }).then((data) => {
-      return data[0];
-    });
+    };
+    if (proxy) opts.proxy = proxy;
+
+    return rp(opts)
+      .then((data) => {
+        return data[0];
+      });
   },
 
-  googleplus: (url) => {
-    return rp.post({
+  googleplus: (url, proxy) => {
+    const opts = {
       url: "https://clients6.google.com/rpc",
       json: {
         method: "pos.plusones.get",
@@ -31,29 +35,36 @@ module.exports = {
         key: "p",
         apiVersion: "v1"
       }
-    }).then((data) => {
-      return data.result.metadata.globalCounts;
-    });
+    };
+    if (proxy) opts.proxy = proxy;
+
+    return rp.post(opts)
+      .then((data) => {
+        return data.result.metadata.globalCounts;
+      });
   },
 
-  moz: (url) => {
-    return rp.post({
+  moz: (url, proxy) => {
+    const opts = {
       url: "http://www.checkmoz.com/",
       form: {
         "f_urls": url
       }
-    }).then((data) => {
-      const $ = cheerio.load(data);
+    };
+    if (proxy) opts.proxy = proxy;
 
-      const metricsRow = $(".rowclass1 td");
+    return rp.post(opts)
+      .then((data) => {
+        const $ = cheerio.load(data);
+        const metricsRow = $(".rowclass1 td");
 
-      return {
-        da: $(metricsRow[1]).text(),
-        pa: $(metricsRow[2]).text(),
-        ranks: $(metricsRow[3]).text(),
-        links: $(metricsRow[4]).text()
-      };
-    });
+        return {
+          da: $(metricsRow[1]).text(),
+          pa: $(metricsRow[2]).text(),
+          ranks: $(metricsRow[3]).text(),
+          links: $(metricsRow[4]).text()
+        };
+      });
   }
 
 };
