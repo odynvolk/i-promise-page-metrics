6d44ab9c8cabfa9b375cb2bc1b5b6728ede22901
Alexi Rahman <alexi.rahman@r76.se>
2017-05-13 20:54:21 +0200

    Added nock for faster tests

--- test/index.js
+++ test/index.js
@@ -1,11 +1,21 @@
 "use strict";
 
-const pageMetrics = require("../index.js");
+const fs = require("fs");
+const nock = require("nock");
+
+const pageMetrics = require("../index");
+
+const checkPrg = fs.readFileSync("./test/data/check-prg-response.html", "utf8");
+const googlePlus = require("./data/google-plus-response.json");
 
 describe("i promise page metrics", () => {
   const url = "https://npmjs.com";
 
   it("should return googleplus data", (done) => {
+    nock("https://clients6.google.com")
+      .post("/rpc")
+      .reply(200, googlePlus);
+
     pageMetrics.googleplus(url)
       .then((response) => {
         response.count.should.be.above(-1);
@@ -15,6 +25,10 @@ describe("i promise page metrics", () => {
   });
 
   it("should return moz data", (done) => {
+    nock("http://www.checkprg.com")
+      .get("/lib/multipr-process.html?u=npmjs.com")
+      .reply(200, checkPrg);
+
     pageMetrics.moz(url)
       .then((response) => {
         response.da.should.be.above(70);
